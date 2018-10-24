775d97b634c509bfdbf51da6f3891cff33057ada
Ravi <projectsaathi@gmail.com>
2018-10-24 17:19:07 +0545

    demo of phpcrud

---
+++ js/myscript.js
@@ -0,0 +1,30 @@
+// function calc()
+// {
+// 	var fristNum = 12;
+// 	var secondNum = 11;
+// 	var add = fristNum + secondNum;
+// 	alert("The Addition is:"+add);
+// }
+
+// // calc();
+
+// function js()
+// {
+// 	var info = {
+// 		"full_name" : "Ravindra Rana",
+// 		"title" : "Student",
+// 		"courses" : [
+// 			"JavaScript & JSON",
+// 			"MFCS",
+// 			"Software Enginerring"
+// 		]
+// 	};
+// }
+
+// js();
+
+// console.log(info.courses[1]);
+
+$("document").ready(function(){
+	alert("Hello Jquery");
+});
